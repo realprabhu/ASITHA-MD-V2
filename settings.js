6b04904f385795c35e5cc4df87b3395b7a4e09a7
realprabhu <114887269+realprabhu@users.noreply.github.com>
2024-12-24 14:53:52 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=66hWDJKC#ABJ4hCZage3rRgS322_ZKfItT5oPb_ICYlDQG29UsZA' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
-ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "කෑවද බං....? 🥲♥️✨ (sapsara prabhu induranga)!*" : process.env.ALIVE_MSG,
+ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "කෑවද බං....? 💞🍃✨ (sapsara prabhu induranga)!*" : process.env.ALIVE_MSG,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
 AUTO_VOICE: process.env.AUTO_VOICE === undefined ? "true" : process.env.AUTO_VOICE,
